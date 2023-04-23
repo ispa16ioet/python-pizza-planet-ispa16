8ad8007201d8bf8be82554d34e71e21ed8df2adf
ispa16ioet <121963908+ispa16ioet@users.noreply.github.com>
2023-04-22 23:04:04 -0500

    refactor code services
    -- inprove code quality
    - remove spaces
    - add utils with reusable functions
    - add new requirements

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,6 +1,5 @@
 from .index import IndexController
 from .ingredient import IngredientController
-from .order import OrderController
-from .size import SizeController
 from .beverage import BeverageController
+from .size import SizeController
 from .report import ReportController
