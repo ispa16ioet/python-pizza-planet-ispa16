af11ee7e9a0baf02f2716ec49d46e95759581e55
ispa16ioet <121963908+ispa16ioet@users.noreply.github.com>
2023-04-22 22:46:25 -0500

    refactor to state pattern
    --complete logic for state pattern
    --refactor create order
    -refactor create service

---
+++ app/order/order_state.py
@@ -0,0 +1,121 @@
+from sqlalchemy.exc import SQLAlchemyError
+from datetime import datetime
+
+from ..common.utils import check_required_keys
+from ..repositories.managers import (IngredientManager, OrderManager,
+                                     SizeManager, BeverageManager)
+from ..controllers.base import BaseController
+
+class OrderState(object):
+    def __init__(self, context):
+        self.context = context
+    def updateState(self):
+        try:
+            self.order_detail = self.manager.update(_id=self.id,new_values= {'state':self.name})
+            self.order_error = None
+        except (SQLAlchemyError, RuntimeError) as ex:
+            self.order_detail = None
+            self.order_error =str(ex)
+   
+
+class Default(OrderState):
+    name ='Default'
+    manager = OrderManager
+    order_detail =''
+    order_error = ''
+
+    def get_all_orders(self):
+        
+        try:
+            self.order_detail = self.manager.get_all()
+            self.order_error = None
+        except (SQLAlchemyError, RuntimeError) as ex:
+            self.order_detail = None
+            self.order_error =str(ex)
+
+    def get_order_by_id(self,_id):
+
+        try:
+            self.order_detail = self.manager.get_by_id(_id)
+            self.order_error = None
+        except (SQLAlchemyError, RuntimeError) as ex:
+            self.order_detail = None
+            self.order_error =str(ex)
+
+
+
+class CreateOrder(OrderState):
+    order_detail =''
+    order_error = ''
+    name ='OrderRealized'
+    manager = OrderManager
+    __required_info = ('client_name', 'client_dni', 'client_address', 'client_phone', 'size_id')
+    
+    def calculate_order_price(self,size_price: float, ingredients: list, beverages: list):
+        price = sum(ingredient.price for ingredient in ingredients)+size_price +sum(beverage.price for beverage in beverages)
+        return round(price, 2)
+
+    def create(self, order: dict):
+        current_order = order.copy()
+        if not check_required_keys(self.__required_info, current_order):
+            return 'Invalid order payload', None
+       
+        if not current_order.get('state'):
+            current_order['state'] = self.name
+
+        if current_order.get('date'):
+            current_order['date']= datetime.strptime(current_order['date'], "%Y-%m-%d %H:%M:%S.%f")
+        
+        size_id = current_order.get('size_id')
+        size = SizeManager.get_by_id(size_id)
+
+        if not size:
+            return 'Invalid size for Order', None
+
+        ingredient_ids = current_order.pop('ingredients', [])
+        beverage_ids = current_order.pop('beverages', [])
+        try:
+            ingredients = IngredientManager.get_by_id_list(ingredient_ids)
+            beverages = BeverageManager.get_by_id_list(beverage_ids)
+            price = self.calculate_order_price(size.get('price'), ingredients,beverages)
+            order_with_price = {**current_order, 'total_price': price}
+            self.order_detail = self.manager.create(order_data = order_with_price, ingredients= ingredients, beverages =beverages)
+            self.order_error =None 
+        except (SQLAlchemyError, RuntimeError) as ex:
+            self.order_detail = None
+            self.order_error =str(ex)
+
+
+
+class OnPreparing(OrderState):
+    
+    def __init__(self,order_detail):
+        self.order_detail = order_detail.order_detail
+        self.manager = OrderManager
+        self.name = "OnPreparing"
+        self.order_detail['state']=self.name
+        self.id = order_detail.order_detail['_id']
+        self.updateState()
+
+
+
+
+class Sended(OrderState):
+    def __init__(self,order_detail):
+        self.order_detail = order_detail.order_detail
+        self.manager = OrderManager
+        self.name = "Sended"
+        self.order_detail['state']=self.name
+        self.id = order_detail.order_detail['_id']
+        self.updateState()
+
+
+class Finish(OrderState):
+    def __init__(self,order_detail):
+        self.order_detail = order_detail.order_detail
+        self.manager = OrderManager
+        self.name = "Finish"
+        self.order_detail['state']=self.name
+        self.id = order_detail.order_detail['_id']
+        self.updateState()
+
--- app/services/order.py
+++ app/services/order.py
@@ -2,15 +2,18 @@ from app.common.http_methods import GET, POST
 from flask import Blueprint, jsonify, request
 
 from ..controllers import OrderController
-
+from ..order.order import Order
+from ..order.order_state import CreateOrder
 order = Blueprint('order', __name__)
 
 
 @order.route('/', methods=POST)
 def create_order():
-    order, error = OrderController.create(request.json)
-    response = order if not error else {'error': error}
-    status_code = 200 if not error else 400
+    new_order = Order()
+    new_order.set_state(CreateOrder(new_order))
+    new_order.create_order(request.json)
+    response = new_order.order_detail if not new_order.order_error else {'error': new_order.order_error}
+    status_code = 200 if not new_order.order_error else 400
     return jsonify(response), status_code
 
 
@@ -25,6 +28,7 @@ def get_order_by_id(_id: int):
 @order.route('/', methods=GET)
 def get_orders():
     orders, error = OrderController.get_all()
+    print()
     response = orders if not error else {'error': error}
     status_code = 200 if orders else 404 if not error else 400
     return jsonify(response), status_code
